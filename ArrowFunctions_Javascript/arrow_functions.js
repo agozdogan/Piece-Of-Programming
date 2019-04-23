9c5f73140465ad1342fac2275eb6a7e76c57149a
Görkem Özdoğan <agozdogan@gmail.com>
2019-04-23 16:57:42 +0300

    Programming Samples

---
+++ ArrowFunctions_Javascript/arrow_functions.js
@@ -0,0 +1,10 @@
+// https://morioh.com/p/7639c262cbed/javascript-s-arrow-functions-explained-by-going-down-a-slide
+
+function addTen(num){
+    return num + 10; 
+}
+
+console.log(addTen(10));
+
+let addTenT = (num) => num + 10; 
+console.log(addTenT(19));
