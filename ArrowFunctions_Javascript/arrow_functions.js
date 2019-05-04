8d9a65d36115fa4bc9781158ecb4259228f96c53
Görkem Özdoğan <agozdogan@gmail.com>
2019-05-04 11:04:02 +0300

    vs extensions test

--- ArrowFunctions_Javascript/arrow_functions.js
+++ ArrowFunctions_Javascript/arrow_functions.js
@@ -1,20 +1,21 @@
 // https://morioh.com/p/7639c262cbed/javascript-s-arrow-functions-explained-by-going-down-a-slide
 
-function addTen(num){
-    return num + 10; 
+function addTen(num) {
+  return num + 10;
 }
 
 console.log(addTen(10));
 
-let addTenT = (num) => num + 10; 
+let addTenT = num => num + 10;
 console.log(addTenT(19));
 
-let nums = [1,4,9];
-let squares = nums.map((num) => {
-    return Math.sqrt(num);
+let nums = [1, 4, 9];
+let squares = nums.map(num => {
+  return Math.sqrt(num);
 });
 console.log(squares);
 
-let multiply = (num1,num2) => {return num1 * num2};
-console.log(multipl(2,10)) ;
-
+let multiply = (num1, num2) => {
+  return num1 * num2;
+};
+console.log(multipl(2, 10));
