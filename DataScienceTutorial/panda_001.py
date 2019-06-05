c24b2b395b85fee499584de1913b4a544c339932
Görkem Özdoğan <agozdogan@gmail.com>
2019-06-05 16:17:47 +0300

    data science works  for python

---
+++ DataScienceTutorial/panda_001.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns  # visualization tool
+
+data = pd.read_csv("DataScienceTutorial\pokemon.csv")
+#print(data.corr())
+
+""" data.Speed.plot(kind = 'line', color = 'g',label = 'Speed',linewidth=1,alpha = 0.5,grid = True,linestyle = ':')
+data.Defense.plot(color = 'r',label = 'Defense',linewidth=1, alpha = 0.5,grid = True,linestyle = '-.')
+plt.legend(loc='upper right')     # legend = puts label into plot
+plt.xlabel('x axis')              # label = name of label
+plt.ylabel('y axis')
+plt.title('Line Plot')            # title = title of plot
+plt.show() """
+
+# x = data['Defense']>200
+# print(data[x])
+
+print(data.head())
+
+print(data['Type 1'].value_counts(dropna = False))
+
+i = 0 
+while i!= 5 : 
+    print ( 'i is :', i)
+    i += 1 
+print(i, "is equal to 5")    
+
+dic = {"spain":"madrid", "spain":"barcelona","portugal":"lisbon"}
+for key,value in dic.items():
+    print(key, ':', value)
+
+liste = [1,2,3,4,5]
+
+for i in liste:
+    print('i is :', i)
---
+++ DataScienceTutorial/python_function.py
@@ -0,0 +1,17 @@
+number_list = [1,2,3]
+y = map(lambda x:x**2, number_list)
+print(list(y))
+
+name ="görkem"
+it = iter(name)
+print(*it)
+
+list1 = [8,2,3]
+list2 = [48,62,63]
+
+z = zip(list1,list2)
+
+z_list= list(z)
+
+print(z_list)
+
